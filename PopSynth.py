387becfb825bcef938ccef5925ad3aa380a10aa5
stegmaja <58080521+stegmaja@users.noreply.github.com>
2023-10-02 17:00:29 +0200

    Started to add the kicks

--- PopSynth.py
+++ PopSynth.py
@@ -2,19 +2,18 @@ from cosmic.sample.initialbinarytable import InitialBinaryTable
 from cosmic.evolve import Evolve
 import numpy as np
 
-BSEDict = {'xi': 1.0, 'bhflag': 0, 'neta': 0.5, 'windflag': 3, 'wdflag': 1, 'alpha1': 1.0, 'pts1': 0.001, 'pts3': 0.02, 'pts2': 0.01, 'epsnov': 0.001, 'hewind': 0.5, 'ck': 1000, 'bwind': 0.0, 'lambdaf': 0.0, 'mxns': 3.0, 'beta': -1.0, 'tflag': 1, 'acc2': 1.5, 'grflag' : 1, 'remnantflag': 4, 'ceflag': 0, 'eddfac': 1.0, 'ifflag': 0, 'bconst': 3000, 'sigma': 265.0, 'gamma': -2.0, 'pisn': 45.0, 'natal_kick_array' : [[-100.0,-100.0,-100.0,-100.0,0.0], [-100.0,-100.0,-100.0,-100.0,0.0]], 'bhsigmafrac' : 1.0, 'polar_kick_angle' : 90, 'qcrit_array' : [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0], 'cekickflag' : 2, 'cehestarflag' : 0, 'cemergeflag' : 0, 'ecsn' : 2.25, 'ecsn_mlow' : 1.6, 'aic' : 1, 'ussn' : 0, 'sigmadiv' :-20.0, 'qcflag' : 1, 'eddlimflag' : 0, 'fprimc_array' : [2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0], 'bhspinflag' : 0, 'bhspinmag' : 0.0, 'rejuv_fac' : 1.0, 'rejuvflag' : 0, 'htpmb' : 1, 'ST_cr' : 1, 'ST_tide' : 1, 'bdecayfac' : 1, 'rembar_massloss' : 0.5, 'kickflag' : 0, 'zsun' : 0.014, 'bhms_coll_flag' : 0, 'don_lim' : -1, 'acc_lim' : -1, 'rtmsflag' : 0}
+BSEDict = {'xi': 1.0, 'bhflag': 2, 'neta': 0.5, 'windflag': 3, 'wdflag': 1, 'alpha1': 1.0, 'pts1': 0.001, 'pts3': 0.02, 'pts2': 0.01, 'epsnov': 0.001, 'hewind': 0.5, 'ck': 1000, 'bwind': 0.0, 'lambdaf': 0.0, 'mxns': 3.0, 'beta': -1.0, 'tflag': 1, 'acc2': 1.5, 'grflag' : 1, 'remnantflag': 4, 'ceflag': 0, 'eddfac': 1.0, 'ifflag': 0, 'bconst': 3000, 'sigma': 265.0, 'gamma': -2.0, 'pisn': 45.0, 'natal_kick_array' : [[-100.0,-100.0,-100.0,-100.0,0.0], [-100.0,-100.0,-100.0,-100.0,0.0]], 'bhsigmafrac' : 1.0, 'polar_kick_angle' : 90, 'qcrit_array' : [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0], 'cekickflag' : 2, 'cehestarflag' : 0, 'cemergeflag' : 0, 'ecsn' : 2.25, 'ecsn_mlow' : 1.6, 'aic' : 1, 'ussn' : 0, 'sigmadiv' :-20.0, 'qcflag' : 1, 'eddlimflag' : 0, 'fprimc_array' : [2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0], 'bhspinflag' : 0, 'bhspinmag' : 0.0, 'rejuv_fac' : 1.0, 'rejuvflag' : 0, 'htpmb' : 1, 'ST_cr' : 1, 'ST_tide' : 1, 'bdecayfac' : 1, 'rembar_massloss' : 0.5, 'kickflag' : 0, 'zsun' : 0.014, 'bhms_coll_flag' : 0, 'don_lim' : -1, 'acc_lim' : -1, 'rtmsflag' : 0}
 
+# Set the initial parameters
 m10 = 85.543645
 m20 = 84.99784
-m30 = 25.543645
+m30 = 5.543645
+sep30 = 9e3 # solar radii
+ecc3 = 0.
 
 # Initiate inner binary and tertiary companion
 single_binary   = InitialBinaryTable.InitialBinaries(m1=m10, m2=m20, porb=446.795757, ecc=0.448872, tphysf=13700.0, kstar1=1, kstar2=1, metallicity=0.002)
 single_tertiary = InitialBinaryTable.InitialBinaries(m1=m30, m2=0.1, porb=1e12, ecc=0., tphysf=13700.0, kstar1=1, kstar2=14, metallicity=0.002)
-
-# Set the initial outer orbital parameters
-sep30 = 9e3 # solar radii
-ecc3 = 0.
 
 # Evolve tertiary
 bpp_tertiary, bcm_tertiary, initC_tertiary, kick_info_tertiary = Evolve.evolve(initialbinarytable=single_tertiary, BSEDict=BSEDict, dtp=1)
@@ -40,8 +39,7 @@ if(len(bcm[bcm['RRLO_3']>1])>0):
 if(len(bcm[bcm['Stability']<1])>0):
     print("System is not hierarchically stable")
 
-
-# Check for systems that form an isolated DCO, and inner DCO that remains stable and not tertiary RLO-filling
+# Check for systems that form a stellar merger, isolated DCO, and inner DCO that remains stable and not tertiary RLO-filling
 Stability = (len(bcm[bcm['Stability']<1])==0)
 Detached = (len(bcm[bcm['RRLO_3']>1])==0)
 LowMass = (len(bcm[bcm['kstar_3']>=13])==0)
@@ -72,3 +70,28 @@ if(len(TCO)>0):
 if(len(DCOLowMass)>0):
     print("Inner DCO plus low mass companion have been formed:")
     print(DCOLowMass[['tphys','kstar_1','kstar_2','kstar_3','mass_1','mass_2','mass_3','sep','sep3','ecc','ecc3','bin_state']].iloc[0])
+
+print(kick_info)
+
+# Get the kick times, and their order
+t1 = bpp[bpp['evol_type']==15]['tphys']
+t2 = bpp[bpp['evol_type']==16]['tphys']
+t3 = bpp_tertiary[bpp_tertiary['evol_type']==15]['tphys']
+tSN = np.array([])
+iSN = np.array([])
+if(len(t1)>0):
+    tSN = np.append(tSN,t1)
+    iSN = np.append(iSN,1)
+if(len(t2)>0):
+    tSN = np.append(tSN,t2)
+    iSN = np.append(iSN,2)
+if(len(t3)>0):
+    tSN = np.append(tSN,t3)
+    iSN = np.append(iSN,3)
+iSN = iSN[np.argsort(tSN)]
+tSN = np.sort(tSN)
+
+# Apply the kicks
+for i in iSN:
+    if(i<=2): # SN occured in the inner binary
+        print("...")
